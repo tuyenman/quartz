6648111d9b1bfc664f215cecaa1b3637bd4c713d
tuyenman <nguyenthanhtuyen.kts@gmail.com>
2024-10-15 23:16:54 +0700

    Initial Quartz site

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "tuyenman.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
